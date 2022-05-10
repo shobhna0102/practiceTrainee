f9efddfbf83d3b38f55e65cd4d08e7334f78ce3b
shobhna0102 <shobhnabaraiya@webcluesinfotech.com>
2022-05-10 15:01:29 +0530

    Initial commit

---
+++ shobhna.js
@@ -0,0 +1,153 @@
+
+//var nm="shobhna"
+//let age =30;
+//let name=true;
+//let s1 ;
+//console.log(nm);
+//console.log(age);
+//console.log('name');
+//console.log(typeof(s1));
+
+//object
+//change name of the object value 2 ways
+// let person={
+//     name1:"shobhna",
+//     age:20
+// };
+//person.name1='bhumi';
+//rson['name1']='nayus';
+//console.log(person);
+//console.log(person.name1);
+
+//array
+//let color=['red','blue'];
+//console.log(color);
+//console.log(color.length);
+
+//function add(name,lname)
+//{
+    //console.log('helloo..'+ name+'  '+ lname);
+//}
+//add('shobha','baraiya');
+
+//let r =20;
+//const pi=3.14;
+//let ans;
+//ans = pi*r*r;
+//console.log(ans);
+
+//    let num1=2;
+//    let num2=3;
+//    let num3=5;
+
+//    if(num1>num2 && num1>num3)
+//    {
+//           console.log('number i is greater');
+//    }
+//    else
+//          console.log('number 2 is greater');
+
+
+// let num=10;
+// let ans= num%2===0 ? "even":"odd"
+// console.log(ans);
+
+// let day='mon';
+// switch(day)
+// {
+//     case 'mon':
+//         console.log("7am");
+//             break;
+//     case 'tues':
+//         console.log("8am");
+//             break;
+//     case 'wed':
+//         console.log("9am");
+//              break;
+//     case 'thur':
+//         console.log("5am");
+//             break;
+//     case 'fri':
+//         console.log("6am");
+//              break;
+//     default:
+//         console.log("not...");
+// }
+
+    // 
+    
+// let x=10;
+//     if(x>0)
+// {
+//     document.write("positive");
+
+//     if(x%2==0)
+//     {
+//         document.write("positive but EVEN");
+
+//     }
+//     else
+//     {
+//         document.write("positive but ODD");
+
+//     }
+
+// }
+// else
+// {
+//     document.write("negative");
+// }
+
+// let x=20;
+//  if(x>0 && x%2==0)
+//  {
+//     document.write("positive and EVEN");
+//  }
+//  else
+//  {
+//      document.write("negative and ODD");
+//  }
+
+// let x=5;
+// for(let i=1; i<=10; i++)
+// {
+//     console.log(x+"*"+i+"="+x*i);   
+// }
+
+// let x=6;
+// let i=1;
+// while ( i<=10 ) 
+// {
+//     console.log(x+"*"+i+"="+x*i);
+//     i++;
+// }
+
+
+// let x=7;
+// let i=1;
+// do
+// {
+//     console.log(x+"*"+i+"="+x*i);
+//     i++;
+
+// } while(i<=10)
+
+
+// function add(a,b)
+// {
+//         return a+b;
+// }
+// console.log(add(5,6));
+
+// let color=['red','pink','black','green'];
+// console.log(typeof(color));
+// console.log(color[1]);
+// console.log(color[4]);
+
+// let color=['red','pink','black','green'];
+// let
+// for  i in color
+// {
+//     console.log(color[i]);
+// }
+
